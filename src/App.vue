6647b2f4b99a10468eaa80f320670d1e7a77b9dc
Abolfazl <a.baghshahi1327@gmail.com>
2021-05-01 01:24:49 +0430

    Revert "add update note"
    This reverts commit ff9241811c08159ebcd517c8d1d5cf890ebd5445.

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,19 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <Header />
-    <router-view
-      :data="data"
-      @delete-item="handleDelete($event)"
-      @new-note="handleNewNote($event)"
-      @update:note="handelUpdate($event)"
-    ></router-view>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import Header from "./components/header";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    // HelloWorld
-    Header,
-  },
-
-  data() {
-    return {
-      data: [
-        {
-          id: 1,
-          title: "title1",
-          content: ["first", "second"],
-        },
-        {
-          id: 2,
-          title: "title2",
-          content: ["first", "second"],
-        },
-      ],
-    };
-  },
-  methods: {
-    handleDelete(id) {
-      this.data = this.data.filter((item) => item.id !== id);
-    },
-    handleNewNote(note) {
-      this.data.push(note);
-    },
-    handelUpdate(note) {
-      console.log(note.id);
-      console.log(this.data[1].id);
-      for (var i in this.data) {
-        if (this.data[i].id == note.id) {
-          this.data[i] = note;
-          alert('hi')
-          break; //Stop this loop, we found it!
-        }
-      }
-    },
-  },
-};
+    HelloWorld
+  }
+}
 </script>
 
 <style>
@@ -68,5 +23,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- src/utils/random.js
+++
@@ -1,7 +0,0 @@
-const random = (range) => {
-    const rand = Math.random()
-    const rangedRand = rand * range
-    return Math.ceil(rangedRand)
-}
-
-export default random
